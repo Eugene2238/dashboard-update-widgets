346299a79981ced2f2fcf0d072b03716cacc74a3
evgenii <eugene2238@gmail.com>
2019-07-21 15:33:37 -0700

    Fixed bug with URL library

--- widget-services/specific-text.js
+++ widget-services/specific-text.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const https = require('https');
+const URL = require('url').URL;
 
 module.exports.specificText = function(widget, callback) {
     const url = new URL(widget.settings.url);
